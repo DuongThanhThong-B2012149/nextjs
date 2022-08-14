b5fcd46e81c70749ae87bf94d75dff179adc352e
thanhthong <thongduong61134@gmail.com>
2022-08-14 14:47:42 +0700

    SSG getStaticProps with ts

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,29 @@
+import axios from 'axios'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface Props {}
 
 const About = (props: Props) => {
   const router = useRouter()
+  // const [posts, setPosts] = useState([])
+  // useEffect(() => {
+  //   ;(async () => {
+  //     const data = await axios.get('http://js-post-api.herokuapp.com/api/posts')
+  //     setPosts(data.data)
+  //   })()
+  // }, [])
   console.log(router.query)
-  return <div>About</div>
+
+  return (
+    <div>
+      {/* <ul>
+        {posts.map((x: any, i: number) => (
+          <li key={i}>{x.title}</li>
+        ))}
+      </ul> */}
+    </div>
+  )
 }
 
 // export async function getServerSideProps() {
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,9 +1,34 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next'
 import React from 'react'
 
-interface Props {}
+interface Props {
+  posts: any[]
+}
 
-const PostListPage = (props: Props) => {
-  return <div>PostListPage</div>
+const PostListPage = ({ posts }: Props) => {
+  return (
+    <div>
+      <h1>Post List Page</h1>
+      <ul>
+        {posts.map((post: any, index: any) => (
+          <li key={index}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => {
+  // Server-side
+  // Build time
+  console.log('Static props')
+  const response = await fetch('http://js-post-api.herokuapp.com/api/posts')
+  const data: any[] = await response.json()
+  return {
+    props: {
+      posts: data.map((x: any) => ({ id: x.id, title: x.title })),
+    },
+  }
 }
 
 export default PostListPage
