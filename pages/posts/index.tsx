5e2641d764b7116b88bcfbc9c771ec5a65a43d98
thanhthong <thongduong61134@gmail.com>
2022-08-14 18:57:17 +0700

    getStaticPaths with ts

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,32 +1,39 @@
 import { GetStaticProps, GetStaticPropsContext } from 'next'
+import Link from 'next/link'
 import React from 'react'
 
-interface Props {
+interface PostListPageProps {
   posts: any[]
 }
 
-const PostListPage = ({ posts }: Props) => {
+const PostListPage = ({ posts }: PostListPageProps) => {
   return (
     <div>
       <h1>Post List Page</h1>
       <ul>
         {posts.map((post: any, index: any) => (
-          <li key={index}>{post.title}</li>
+          <Link key={index} href={`/posts/${post.id}`} prefetch={false}>
+            <li>
+              <a>{post.title}</a>
+            </li>
+          </Link>
         ))}
       </ul>
     </div>
   )
 }
 
-export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => {
+export const getStaticProps: GetStaticProps<PostListPageProps> = async (
+  context: GetStaticPropsContext
+) => {
   // Server-side
   // Build time
   console.log('Static props')
-  const response = await fetch('http://js-post-api.herokuapp.com/api/posts')
-  const data: any[] = await response.json()
+  const response = await fetch('http://js-post-api.herokuapp.com/api/posts?_page=1')
+  const data: any = await response.json()
   return {
     props: {
-      posts: data.map((x: any) => ({ id: x.id, title: x.title })),
+      posts: data?.data.map((x: any) => ({ id: x.id, title: x.title })),
     },
   }
 }
