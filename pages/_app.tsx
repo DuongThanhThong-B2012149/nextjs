967ff2512de60be48b983493f3b73d18a183fffb
thanhthong <thongduong61134@gmail.com>
2022-08-20 22:39:06 +0700

    video-23

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,22 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AppPropsWithLayout } from '../models'
 import { EmptyLayout } from '@/components/common/layout'
+import { SWRConfig } from 'swr'
+import axiosClient from '@/api/axios-client'
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const Layout = Component.Layout ?? EmptyLayout
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SWRConfig
+      value={{
+        fetcher: (url) => axiosClient.get(url),
+        shouldRetryOnError: false,
+      }}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SWRConfig>
   )
 }
 
